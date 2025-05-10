0cd8d2bfee9affd8ec7d87d1dd26e6a92e385bb3
juanjoseguerrer <jjguerreroa@ufpso.edu.co>
2025-05-10 09:09:00 -0500

    Pasando archivos de la clase pasada al nuevo repo

---
+++ js/loginService.js
@@ -0,0 +1,66 @@
+document.getElementById('loginForm').addEventListener('submit', function(e){
+    e.preventDefault();
+    const email = document.getElementById('email').value
+    const password = document.getElementById("password").value
+    console.log("valores ledios del formulario",  {email, password})
+    login(email, password)
+})
+
+function login(email, password){
+    let message = '';
+    let alertType = '';
+    const LOGIN_ENDPOINT = 'https://reqres.in/api/login'
+    fetch(LOGIN_ENDPOINT, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": 'reqres-free-v1'
+        },
+        body: JSON.stringify({email, password})
+    })
+
+    .then((response) => {
+        return response.json().then(
+            data => {
+                return {
+                    status: response.status,
+                    data
+                }
+            }
+        )
+    })
+
+    .then((result) => {
+        if(result.status == 200){
+            alertType = 'success'
+            message = 'Inicio de sesión correcto'
+            alertBuilder(alertType, message)
+        }
+        else{
+            alertType = 'danger'
+            message = 'Correo o contraseña incorrectos'
+            alertBuilder(alertType, message)
+        }
+    })
+
+    .catch((error) =>{
+        alertType = 'danger'
+        message = 'Errorinesperado' + error
+        alertBuilder(alertType, message)
+    })
+
+
+
+
+}   
+
+function alertBuilder(alertType, message){ 
+    const alert = `
+        <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    `
+
+    document.getElementById('alert').innerHTML = alert
+ }
